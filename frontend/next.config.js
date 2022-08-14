282ad65276d14362f2370cdcb46fe2c000b3a195
paolocalzone <paolocalzone@protonmail.com>
2022-08-14 23:36:17 +0200

    Edit

--- frontend/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  images: {
-    domains: ["images.unsplash.com", "ipfs.io"],
-  },
-  reactStrictMode: false,
-};
--- frontend/utils/joinClassNames.js
+++
@@ -1,5 +0,0 @@
-function joinClassNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
-export default joinClassNames;
