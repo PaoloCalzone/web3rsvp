78947354af392b824df49e0cfc45e7c58d3e7e56
paolocalzone <paolocalzone@protonmail.com>
2022-08-15 15:14:12 +0200

    Set up boilerplate for client and create wallet connection with Rainbowkit

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Navmenu from "./Navmenu";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect } from "wagmi";
 
 export default function Navbar() {
+  const { data: account } = useAccount();
+  const { disconnect } = useDisconnect();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -29,6 +33,11 @@ export default function Navbar() {
                 </a>
               </Link>
             </div>
+            {account ? (
+              <Navmenu account={account} disconnect={() => disconnect()} />
+            ) : (
+              <ConnectButton />
+            )}
           </div>
         </nav>
       </header>
