969118a499140e994ebbba149cdbb146a7293d54
paolocalzone <paolocalzone@protonmail.com>
2022-08-20 15:11:26 +0200

    Rsvp to Events

--- frontend/pages/event/[id].js
+++ frontend/pages/event/[id].js
@@ -1,3 +1,9 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import connectContract from "../../utils/connectContract";
+import Alert from "../../components/Alert";
 import Head from "next/head";
 import { gql } from "@apollo/client";
 import client from "../../apollo-client";
@@ -12,7 +18,56 @@ import {
 } from "@heroicons/react/outline";
 
 function Event({ event }) {
+  const { data: account } = useAccount();
+  const [success, setSuccess] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [currentTimestamp, setEventTimestamp] = useState(new Date().getTime());
+
   console.log("EVENT:", event);
+
+  function checkIfAlreadyRSVPed() {
+    if (account) {
+      for (let i = 0; i < event.rsvps.length; i++) {
+        const thisAccount = account.address.toLowerCase();
+        if (event.rsvps[i].attendee.id.toLowerCase() == thisAccount) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  async function newRSVP() {
+    try {
+      const rsvpContract = connectContract();
+      if (rsvpContract) {
+        const txn = await rsvpContract.createNewRSVP(event.id, {
+          value: event.deposit,
+          gasLimit: 300000,
+        });
+        setLoading(true);
+        console.log("LOADING...", loading);
+        console.log("Minting transaction transaction:", txn.hash);
+
+        await txn.wait();
+        console.log("Transaction minted...", txn.hash);
+        setSuccess(true);
+        setLoading(false);
+
+        setMessage(`You've successfully rsvped for ${event.name}`);
+      } else {
+        console.log("Error getting contract.");
+      }
+    } catch (err) {
+      setSuccess(false);
+      setMessage("Something went wrong with your transaction...");
+      setLoading(false);
+      console.log("Success:", success);
+      console.log(err);
+    }
+  }
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Head>
@@ -21,6 +76,30 @@ function Event({ event }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="relative py-12">
+        {loading && (
+          <Alert
+            alertType={"loading"}
+            alertBody={"Please wait"}
+            triggerAlert={true}
+            color={"white"}
+          />
+        )}
+        {success && (
+          <Alert
+            alertType={"success"}
+            alertBody={message}
+            triggerAlert={true}
+            color={"palegreen"}
+          />
+        )}
+        {success === false && (
+          <Alert
+            alertType={"failed"}
+            alertBody={message}
+            triggerAlert={true}
+            color={"palevioletred"}
+          />
+        )}
         <h6 className="mb-2">{formatTimestamp(event.eventTimestamp)}</h6>
         <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl mb-6 lg:mb-12">
           {event.name}
@@ -45,6 +124,7 @@ function Event({ event }) {
               <TicketIcon className="w-6 mr-2" />
               <span className="truncate">1 RSVP per wallet</span>
             </div>
+
             <div className="flex items-center">
               <EmojiHappyIcon className="w-10 mr-2" />
               <span className="truncate">
@@ -58,6 +138,44 @@ function Event({ event }) {
                   {event.eventOwner}
                 </a>
               </span>
+            </div>
+            <div className="flex items-center">
+              <div>
+                {event.eventTimestamp > currentTimestamp ? (
+                  account ? (
+                    checkIfAlreadyRSVPed() ? (
+                      <>
+                        <span className="w-full text-center px-6 py-3 text-base font-medium rounded-full text-teal-800 bg-teal-100">
+                          You have RSVPed! 🙌
+                        </span>
+                        <div className="flex item-center">
+                          <LinkIcon className="w-6 mr-2 text-indigo-800" />
+                          <a
+                            className="text-indigo-800 truncate hover:underline"
+                            href={event.link}
+                          >
+                            {event.link}
+                          </a>
+                        </div>
+                      </>
+                    ) : (
+                      <button
+                        type="button"
+                        className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={newRSVP}
+                      >
+                        RSVP for {ethers.utils.formatEther(event.deposit)} MATIC
+                      </button>
+                    )
+                  ) : (
+                    <ConnectButton />
+                  )
+                ) : (
+                  <span className="w-full text-center px-6 py-3 text-base font-medium rounded-full border-2 border-gray-200">
+                    Event has ended
+                  </span>
+                )}
+              </div>
             </div>
           </div>
         </div>
