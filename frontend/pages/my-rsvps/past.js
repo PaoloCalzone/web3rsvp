9e640e02e2d8c70f5a597f572438076fbd5ed7e3
paolocalzone <paolocalzone@protonmail.com>
2022-08-22 00:06:51 +0200

    Add conditionnal statement for first user

--- frontend/pages/my-rsvps/past.js
+++ frontend/pages/my-rsvps/past.js
@@ -7,12 +7,9 @@ import EventCard from "../../components/EventCard";
 
 export default function MyPastRSVPs() {
   const { data: account } = useAccount();
-  // To lower case because some providers use UpperCase form some letters
-  const id = account ? account.address.toLowerCase() : "";
 
-  const [currentTime, setCurrentTime] = useState(
-    new Date().getTime().toString()
-  );
+  const id = account ? account.address.toLowerCase() : "";
+  const [currentTimestamp, setEventTimestamp] = useState(new Date().getTime());
   const { loading, error, data } = useQuery(MY_PAST_RSVPS, {
     variables: { id },
   });
@@ -20,39 +17,41 @@ export default function MyPastRSVPs() {
   if (loading)
     return (
       <Dashboard page="rsvps" isUpcoming={false}>
-        Loading...
+        <p>Loading...</p>
       </Dashboard>
     );
-
   if (error)
     return (
       <Dashboard page="rsvps" isUpcoming={false}>
-        `Error: ${error.message}`
+        <p>`Error! ${error.message}`</p>
       </Dashboard>
     );
-  // if (data) console.log(data);
+  if (data) console.log(data);
+
   return (
     <Dashboard page="rsvps" isUpcoming={false}>
       {account ? (
         <div>
-          {data && !data.account && <p>You have no past RSVPs yet...</p>}
+          {data && !data.account && <p>No past RSVPs found</p>}
           {data && data.account && (
             <ul
               role="list"
               className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
             >
-              {data.account.rsvps.map((rsvp) => {
-                if (rsvp.eventTimestamp < currentTime) {
+              {data.account.rsvps.map(function (rsvp) {
+                if (rsvp.event.eventTimestamp < currentTimestamp) {
                   return (
                     <li key={rsvp.event.id}>
                       <EventCard
-                        id={rspv.event.id}
+                        id={rsvp.event.id}
                         name={rsvp.event.name}
                         eventTimestamp={rsvp.event.eventTimestamp}
                         imageURL={rsvp.event.imageURL}
                       />
                     </li>
                   );
+                } else {
+                  return <p>No past RSVPs found</p>;
                 }
               })}
             </ul>
@@ -60,9 +59,7 @@ export default function MyPastRSVPs() {
         </div>
       ) : (
         <div className="flex flex-col items-center py-8">
-          <p className="mb-4">
-            Connect your wallet to display your past RSVPs...
-          </p>
+          <p className="mb-4">Please connect your wallet to view your rsvps</p>
           <ConnectButton />
         </div>
       )}
