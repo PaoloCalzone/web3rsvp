429ec4178069b875d0640b0afcd623411ab6f171
paolocalzone <paolocalzone@protonmail.com>
2022-08-24 20:55:00 +0200

    Edit new subgraph endpoint

--- frontend/apollo-client.js
+++ frontend/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/paolocalzone/web3rsvp",
+  uri: "https://api.thegraph.com/subgraphs/name/paolocalzone/web3rsvp-2",
   cache: new InMemoryCache(),
 });
 
